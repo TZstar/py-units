d6fac8511ae94f49e25b823c30cb23db7aa721b2
unknown <zengweihgc@163.com>
2018-07-11 08:58:17 +0800

    更新 pybase 和数据可视化类容

---
+++ 09_数据可视化/seaborn/scatterplot_matrix.py
@@ -0,0 +1,13 @@
+"""
+Scatterplot Matrix
+==================
+
+_thumb: .5, .43
+"""
+import seaborn as sns
+sns.set(style="ticks")
+
+df = sns.load_dataset("iris")
+sns.pairplot(df, hue="species")
+import matplotlib.pyplot as plt
+plt.show()
---
+++ 09_数据可视化/seaborn/timeseries_from_dataframe.py
@@ -0,0 +1,17 @@
+"""
+Timeseries from DataFrame
+=========================
+
+"""
+
+import seaborn as sns
+sns.set(style="darkgrid")
+
+# Load the long-form example gammas dataset
+gammas = sns.load_dataset("gammas")
+
+# Plot the response with standard error
+sns.tsplot(data=gammas, time="timepoint", unit="subject",
+           condition="ROI", value="BOLD signal")
+import matplotlib.pyplot as plt
+plt.show()
